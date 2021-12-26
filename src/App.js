5f4c200ec12212b1dd0f56c83e9dcdb87fdf50ef
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-12-27 01:26:14 +0300

    created Reducers

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/home";
+import Signup from "./components/signup/signup";
+import Header from "./components/header/header";
+import Login from "./components/logIn/login";
+import Forget from "./components/forget/forget";
+import Product from "./components/Product";
+import Profile from "./components/profile/profile";
+import Users from "./components/users/users";
+import OneUser from "./components/oneUser/oneuser";
+import { useSelector } from "react-redux";
+import Notfound from "./components/NotFound/Notfound";
+import NotFoundUn from "./components/notfoundunlog/notfound";
+
+function App() {
+  const state = useSelector((state) => {
+    return state;
+  });
+
+  return (
+    <>
+      <div className="kkk">
+        <img
+          alt="img"
+          className="backg"
+          src="https://images.pexels.com/photos/272745/pexels-photo-272745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        />
+        <div className="bgg"></div>
+        {state.signIn.token && (
+          <>
+            <Header />
+            <Routes>
+              <Route exact path="/home" element={<Home />} />
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/users" element={<Users />} />
+              <Route exact path="/profile/:id" element={<OneUser />} />
+              <Route exact path="/home/Product/:id" element={<Product />} />
+              <Route path="*" element={<Notfound />} />
+            </Routes>
+          </>
+        )}
+        {!state.signIn.token && (
+          <>
+            <Header />
+            <Routes>
+              <Route exact path="/forgetPassword" element={<Forget />} />
+              <Route exact path="/resetPassword" element={<Reset />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/" element={<Login />} />
+              <Route path="*" element={<NotFoundUn />} />
+            </Routes>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
